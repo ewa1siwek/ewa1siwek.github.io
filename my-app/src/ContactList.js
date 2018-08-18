5a947413a4a819638e25e3c784e3c7ca4bc8cfc3
ewa1siwek <ew.siwek@gmail.com>
2018-08-18 12:47:59 +0200

    Display contact list

--- my-app/src/ContactList.js
+++ my-app/src/ContactList.js
@@ -20,70 +20,40 @@ class ContactList extends React.Component{
         ]
     }
 
-    handleSubmit = event => {
-        event.preventDefault()
-
-        this.setState({
-            contacts: this.state.contacts.concat({
-                id: Date.now().toString(32),
-                firstName: this.state.firstNameValue,
-                lastName: this.state.lastNameValue,
-                phoneNumber: this.state.phoneNumberValue,
-                email: this.state.emailValue,
-            })
-        })
-    }
-
     render () {
         return (
-            <React.Fragment>
-                <form onSubmit={this.handleSubmit}>
-                    <input
-                        type="text"
-                        value={this.state.firstNameValue}
-                        onChange={(event) => this.setState({
-                            firstNameValue: event.currentTarget.value
-                        })}
-                    />
-                    <input
-                        type="text"
-                        value={this.state.lastNameValue}
-                        onChange={(event) => this.setState({
-                            lastNameValue: event.currentTarget.value
-                        })}
-                    />
-                    <input
-                        type="text"
-                        value={this.state.phoneNumberValue}
-                        onChange={(event) => this.setState({
-                            phoneNumberValue: event.currentTarget.value
-                        })}
-                    />
-                    <input
-                        type="text"
-                        value={this.state.emailValue}
-                        onChange={(event) => this.setState({
-                            emailValue: event.currentTarget.value
-                        })}
-                    />
-                        <button>Add</button>
+            <div className="ContactList">
+                <form>
+                    <label>imię
+                        <input type="text"/>
+                    </label>
+                    <label>nazwisko
+                        <input type="text"/>
+                    </label>
+                    <label>numer telefonu
+                        <input/>
+                    </label>
+                    <label>e-mail
+                        <input type="email"/>
+                    </label>
+                    <button>dodaj</button>
 
                 <ul>
                     {
                         this.state.contacts.map(
                             contact => (
                                 <li key={contact.id}>
-                                    {contact.firstNameValue}
-                                    {contact.lastNameValue}
-                                    {contact.phoneNumberValue}
-                                    {contact.emailValue}
+                                    {contact.firstName}
+                                    {contact.lastName}
+                                    {contact.phoneNumber}
+                                    {contact.email}
                                 </li>
                             )
                         )
                     }
                 </ul>
                 </form>
-            </React.Fragment>
+            </div>
         )
     }
 }
